49ea91d47d0e37f4af52f7a6cc5434728cf9eb31
Roman Sharkov <roman.scharkov@gmail.com>
2021-06-20 01:16:06 +0200

    Fix: move code to package dir

--- doc.go
+++
@@ -1,5 +0,0 @@
-// Package sched provides a job scheduler implementation
-// that's able to fast-forward time.
-// All methods of both the package and a Scheduler instance
-// are thread-safe and can safely be used from within multiple goroutines.
-package sched
