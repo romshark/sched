d75e5d40d2cc8bc488355ddf3aaab5ae26cbfded
Roman Sharkov <roman.scharkov@gmail.com>
2021-06-21 01:39:28 +0200

    Test: Add test

--- sched/sched_test.go
+++ sched/sched_test.go
@@ -300,6 +300,41 @@ func TestScanAfter(t *testing.T) {
 	ExpectJobsAfter(t, j3)
 }
 
+func TestScanInterrupt(t *testing.T) {
+	mc := gomock.NewController(t)
+	tm := mock.NewMockTimeProvider(mc)
+
+	sched.DefaultScheduler = sched.NewWithProvider(0, tm)
+
+	start := time.Date(2021, 6, 20, 10, 00, 00, 0, time.UTC)
+
+	tm.EXPECT().Now().AnyTimes().Return(start)
+	timer := mock.NewMockTimer(mc)
+	tm.EXPECT().AfterFunc(gomock.Any(), gomock.Any()).AnyTimes().Return(timer)
+
+	callback := func() { panic("this should not be invoked") }
+
+	j1, err := sched.Schedule(sched.Hour, callback)
+	require.NoError(t, err)
+	require.NotZero(t, j1)
+
+	j2, err := sched.Schedule(2*sched.Hour, callback)
+	require.NoError(t, err)
+	require.NotZero(t, j2)
+
+	j3, err := sched.Schedule(3*sched.Hour, callback)
+	require.NoError(t, err)
+	require.NotZero(t, j3)
+
+	count := 0
+	ok := sched.Scan(sched.Job{}, func(job sched.Job, jobFn func()) bool {
+		count++
+		return false
+	})
+	require.True(t, ok)
+	require.Equal(t, 1, count)
+}
+
 type Job struct {
 	sched.Job
 	JobFn func()
